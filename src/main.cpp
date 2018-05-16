8716322b0927843bc2378dc35ad27613164a1d37
Emil Nordén <emilnorden@yahoo.se>
2018-05-16 20:05:37 +0200

    added Range class, currently not working

--- src/main.cpp
+++ src/main.cpp
@@ -9,23 +9,41 @@ template <typename Container>
 void print(Container& c)
 {
     std::cout << "--begin--" << std::endl;
-    for(auto& foo : c)
+
+    auto b = std::begin(c);
+    std::cout << "really begin" << std::endl;
+    auto e = std::end(c);
+
+    
+    for(auto i = b; i != e; ++i)
     {
-        std::cout << foo << std::endl;
+        std::cout << *i << std::endl;
     }
+    // for(auto& foo : c)
+    // {
+    //     std::cout << foo << std::endl;
+    // }
     std::cout << "--end--" << std::endl;
 }
 
 int main() {
-    std::vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9, 29, 100};
+    std::vector<int> v = {1, 2, 3, 4, 5};
 
-    auto result1 = range::where(v, [](int x) { return x % 3 == 0; });
+    auto result1 = range::where(v, [](int x) { return x < 4; });
+    auto result2 = range::skip(result1, 1);
 
-    print(result1);
+    auto range = range::from(v);
+    auto qwe = [](int x) { return x < 4; };
+    auto range2 = range.where(qwe);
 
-    auto result2 = range::where(result1, [](int x) { return x % 2 == 0; });
+    print(range2);
 
-    print(result2);
+
+    // print(result2); // prints 2, 3
+    
+    // v.push_back(-5);
+
+    // print(result2); // prints 2, 3, -5
 
     std::cout << "done!";
 }
--- src/where.h
+++ src/where.h
@@ -1,8 +1,9 @@
-#ifndef WH_H_
-#define WH_H_
+#ifndef WHERE_H_
+#define WHERE_H_
 
 #include <functional>
 #include <iterator>
+#include <iostream>
 
 namespace range
 {
@@ -12,9 +13,10 @@ class WhereIterator : public std::iterator<std::input_iterator_tag, typename TIt
 {
     using TItem = typename TIterator::value_type;
 public:
-    WhereIterator(TIterator iterator, const TIterator iterator_end, const std::function<bool(TItem)>& predicate)
+    WhereIterator(TIterator iterator, TIterator iterator_end, std::function<bool(TItem)>& predicate)
         : m_iterator(iterator), m_iterator_end(iterator_end), m_predicate(predicate)
     {
+        std::cout << "do we reach this?" << std::endl;
         if(m_iterator != m_iterator_end &&
             !m_predicate(*m_iterator))
         {
@@ -51,9 +53,38 @@ public:
     }
 private:
     TIterator m_iterator;
-    const TIterator m_iterator_end;
-    const std::function<bool(TItem)>& m_predicate;
+    TIterator m_iterator_end;
+    std::function<bool(TItem)> m_predicate;
 };
+
+template <typename TIterator>
+class TakeIterator : public std::iterator<std::input_iterator_tag, typename TIterator::value_type>
+{
+    using TItem = typename TIterator::value_type;
+public:
+    TakeIterator(TIterator iterator, TIterator iterator_end, int count)
+        : m_iterator(iterator), m_iterator_end(iterator_end), m_count(count)
+    {
+
+    }
+
+    friend bool operator==(const TakeIterator& a, const TakeIterator& b) 
+    {
+        return a.m_iterator == b.m_iterator;
+    }
+
+    friend bool operator!=(const TakeIterator& a, const TakeIterator& b)
+    {
+        return a.m_iterator != b.m_iterator;
+    }
+
+    TItem& operator*() const { return *m_iterator; }
+private:
+    TIterator m_iterator;
+    const TIterator m_iterator_end;
+    int m_count;
+};
+
 
 template <typename TContainer>
 class WhereEnumerable
@@ -69,19 +100,91 @@ public:
 
     }
 
-    WhereIterator<TInnerIterator> begin()
+    iterator begin()
     {
-        return WhereIterator<TInnerIterator>(m_container.begin(), m_container.end(), m_predicate);
+        return WhereIterator<TInnerIterator>(std::begin(m_container), std::end(m_container), m_predicate);
     }
 
-    WhereIterator<TInnerIterator> end()
+    iterator end()
     {
-        return WhereIterator<TInnerIterator>(m_container.end(), m_container.end(), m_predicate);
+        return WhereIterator<TInnerIterator>(std::end(m_container), std::end(m_container), m_predicate);
     }
 
 private:
     TContainer& m_container;
-    const std::function<bool(TItem)>& m_predicate;
+    std::function<bool(TItem)> m_predicate;
+};
+
+template <typename TContainer>
+class TakeEnumerable
+{
+    using TItem = typename TContainer::value_type;
+    using TInnerIterator = typename TContainer::iterator;
+public:
+    using value_type = typename TContainer::value_type;
+    using iterator = TInnerIterator;
+    TakeEnumerable(TContainer& container, int count)
+        : m_container(container), m_count(count)
+    {
+
+    }
+
+    iterator begin()
+    {
+        return std::begin(m_container);
+    }
+
+    iterator end()
+    {
+        auto end = std::end(m_container);
+        auto it = begin();
+        int count = m_count;
+        while(count > 0 && it != end)
+        {
+            --count;
+            ++it;
+        }
+        return it;
+    }
+private:
+    TContainer& m_container;
+    int m_count;
+};
+
+template <typename TContainer>
+class SkipEnumerable
+{
+    using TItem = typename TContainer::value_type;
+    using TInnerIterator = typename TContainer::iterator;
+public:
+    using value_type = typename TContainer::value_type;
+    using iterator = TInnerIterator;
+    SkipEnumerable(TContainer& container, int count)
+        : m_container(container), m_count(count)
+    {
+
+    }
+
+    iterator begin()
+    {
+        auto end = std::end(m_container);
+        auto it = std::begin(m_container);
+        int count = m_count;
+        while(count > 0 && it != end)
+        {
+            --count;
+            ++it;
+        }
+        return it;
+    }
+
+    iterator end()
+    {
+        return std::end(m_container);
+    }
+private:
+    TContainer& m_container;
+    int m_count;
 };
 
 template <typename TContainer>
@@ -90,6 +193,74 @@ const WhereEnumerable<TContainer> where(TContainer& container, std::function<boo
     return WhereEnumerable(container, predicate);
 }
 
+template <typename TContainer>
+const TakeEnumerable<TContainer> take(TContainer& container, int count)
+{
+    return TakeEnumerable(container, count);
 }
+
+template <typename TContainer>
+const SkipEnumerable<TContainer> skip(TContainer& container, int count)
+{
+    return SkipEnumerable(container, count);
+}
+
+template <typename TContainer>
+class Range
+{
+public:
+    using value_type = typename TContainer::value_type;
+    using iterator = typename TContainer::iterator;
+
+    explicit Range(TContainer& container)
+        : m_container(container)
+    {
+
+    }
+
+    explicit Range(TContainer&& container)
+        : m_container(container)
+    {
+        std::cout << "invoked rvalue ref constructor" << std::endl;
+    }
+
+    iterator begin()
+    {
+        return std::begin(m_container);
+    }
+
+    iterator end()
+    {
+        return std::end(m_container);
+    }
+
+    Range<WhereEnumerable<TContainer>> where(std::function<bool(typename TContainer::value_type)>&& predicate) const
+    {
+        return Range<WhereEnumerable<TContainer>>(WhereEnumerable<TContainer>(m_container, predicate));
+    }
+
+    Range<WhereEnumerable<TContainer>> where2(std::function<bool(typename TContainer::value_type)>& predicate) const
+    {
+        return Range<WhereEnumerable<TContainer>>(WhereEnumerable<TContainer>(m_container, predicate));
+    }
+
+    Range<Range<TContainer>> foo()
+    {
+        return Range<Range<TContainer>>{Range<TContainer>{m_container}};
+    }
+    
+private:
+    TContainer& m_container;
+};
+
+template <typename TContainer>
+const Range<TContainer> from(TContainer& container)
+{
+    return Range(container);
+}   
+
+}
+
+
 
 #endif
