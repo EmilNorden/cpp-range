1bcc447ac88a3f3739e1067aced84c432a63a041
Emil Nordén <emilnorden@yahoo.se>
2018-05-21 20:42:53 +0200

    initial ugly group functionality

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,20 @@
 #include <functional>
 
 #include "print.h"
+#include <string>
 
 using namespace std;
+
+class A
+{
+public:
+    A(int k, std::string&& n)
+        : key(k), name(n)
+    {
+    }
+int key;
+std::string name;
+};
 
 int main()
 {
@@ -17,6 +29,27 @@ int main()
 
     v.push_back(10);
     print(range);
+
+    std::vector<A> v2;
+    v2.push_back(A{30, "Emil"});
+    v2.push_back(A{30, "Johan"});
+    v2.push_back(A{20, "Kalle"});
+    v2.push_back(A{20, "Löken"});
+    v2.push_back(A{35, "Leffe"});
+
+    auto grouped = range::from(v2).group([](A& a) { return a.key; });
+    
+    auto beg = std::begin(grouped);
+
+    for(auto&& group : grouped)
+    {
+        std::cout << "==" << group.key() << "==" << std::endl;
+        for(auto ll : group)
+        {
+            std::cout << ll.name << std::endl;
+        }
+    }
+    
 
     std::cout << "done!";
 }
--- src/range.h
+++ src/range.h
@@ -3,11 +3,54 @@
 
 #include <functional>
 #include <iterator>
+#include <map>
+#include <vector>
 
 #include "print.h"
 
 namespace range
 {
+
+template <typename TIterator>
+class GroupIterator : public std::iterator<std::input_iterator_tag, typename TIterator::value_type>
+{
+public:
+    GroupIterator(TIterator iterator, TIterator iterator_end)
+        : m_iterator(iterator), m_iterator_end(iterator_end)
+    {
+
+    }
+
+    friend bool operator==(const GroupIterator &a, const GroupIterator &b)
+    {
+        return a.m_iterator == b.m_iterator;
+    }
+
+    friend bool operator!=(const GroupIterator &a, const GroupIterator &b)
+    {
+        return a.m_iterator != b.m_iterator;
+    }
+
+    auto key() const -> typename TIterator::value_type::first_type { return m_iterator->first; }
+    // const typename TIterator::value_type& operator*() const { return m_iterator->second; }
+    auto operator*() const -> typename TIterator::value_type::second_type { return m_iterator->second; }
+
+    GroupIterator &operator++()
+    {
+        ++m_iterator;
+        return *this;
+    }
+
+    GroupIterator operator++(int)
+    {
+        GroupIterator tmp = *this;
+        ++m_iterator;
+        return tmp;
+    }
+private:
+    TIterator m_iterator;
+    TIterator m_iterator_end;
+};
 
 template <typename TIterator>
 class WhereIterator : public std::iterator<std::input_iterator_tag, typename TIterator::value_type>
@@ -68,23 +111,23 @@ class WhereEnumerable
   public:
     using value_type = typename TContainer::value_type;
     using iterator = WhereIterator<TInnerIterator>;
-    WhereEnumerable(const TContainer &container, const std::function<bool(TItem)> &predicate)
+    WhereEnumerable(TContainer &container, const std::function<bool(TItem)> &predicate)
         : m_container(container), m_predicate(predicate)
     {
     }
 
-    iterator begin() const
+    iterator begin()
     {
         return WhereIterator<TInnerIterator>(std::begin(m_container), std::end(m_container), m_predicate);
     }
 
-    iterator end() const
+    iterator end()
     {
         return WhereIterator<TInnerIterator>(std::end(m_container), std::end(m_container), m_predicate);
     }
 
   private:
-    const TContainer m_container;
+    TContainer m_container;
     std::function<bool(TItem)> m_predicate;
 };
 
@@ -97,17 +140,17 @@ class TakeEnumerable
   public:
     using value_type = typename TContainer::value_type;
     using iterator = TInnerIterator;
-    TakeEnumerable(const TContainer &container, int count)
+    TakeEnumerable(TContainer &container, int count)
         : m_container(container), m_count(count)
     {
     }
 
-    iterator begin() const
+    iterator begin()
     {
         return std::begin(m_container);
     }
 
-    iterator end() const
+    iterator end()
     {
         auto end = std::end(m_container);
         auto it = begin();
@@ -134,12 +177,12 @@ class SkipEnumerable
   public:
     using value_type = typename TContainer::value_type;
     using iterator = TInnerIterator;
-    SkipEnumerable(const TContainer &container, int count)
+    SkipEnumerable(TContainer &container, int count)
         : m_container(container), m_count(count)
     {
     }
 
-    iterator begin() const
+    iterator begin()
     {
         auto end = std::end(m_container);
         auto it = std::begin(m_container);
@@ -152,15 +195,83 @@ class SkipEnumerable
         return it;
     }
 
-    iterator end() const
+    iterator end()
     {
         return std::end(m_container);
     }
 
   private:
-    const TContainer m_container;
+    TContainer m_container;
     int m_count;
 };
+
+template <typename TContainer, typename GroupType>
+class GroupEnumerable
+{
+    using TItem = typename TContainer::value_type;
+    using TInnerIterator = typename std::map<GroupType, std::vector<TItem>>::iterator;
+
+  public:
+    using value_type = typename TContainer::value_type;
+    using iterator = GroupIterator<TInnerIterator>;
+
+    GroupEnumerable(TContainer &container, std::function<GroupType(value_type&)> group_function)
+        : m_container(container), m_group_function(group_function)
+    {
+    }
+
+    iterator begin()
+    {
+        m_map.clear();
+        for(auto item : m_container)
+        {
+            //TODO: Skapa en egen typ att använda istället för vector, som kan hålla key() också
+            m_map[m_group_function(item)].push_back(item);
+        }
+
+        return GroupIterator<TInnerIterator>(std::begin(m_map), std::end(m_map));
+    }
+
+    iterator end()
+    {
+        return GroupIterator<TInnerIterator>(std::end(m_map), std::end(m_map));
+    }
+
+
+  private:
+    TContainer m_container;
+    std::function<GroupType(value_type&)> m_group_function;
+    std::map<GroupType, std::vector<TItem>> m_map;
+};
+
+template <typename T>
+struct AsFunction : public AsFunction<decltype(&T::operator())>
+{
+};
+
+template <typename ReturnType, typename... Args>
+struct AsFunction<ReturnType(Args...)>
+{
+    using type = std::function<ReturnType(Args...)>;
+};
+
+template <typename ReturnType, typename... Args>
+struct AsFunction<ReturnType (*)(Args...)>
+{
+    using type = std::function<ReturnType(Args...)>;
+};
+
+template <typename Class, typename ReturnType, typename... Args>
+struct AsFunction<ReturnType (Class::*)(Args...) const>
+{
+    using type = std::function<ReturnType(Args...)>;
+};
+
+template <typename F>
+constexpr auto toFunction(F f) -> typename AsFunction<F>::type
+{
+    return {f};
+}
 
 template <typename TContainer>
 class Range
@@ -174,27 +285,27 @@ class Range
     {
     }
 
-    iterator begin() const
+    iterator begin()
     {
         return std::begin(m_container);
     }
 
-    iterator end() const
+    iterator end()
     {
         return std::end(m_container);
     }
 
-    Range<WhereEnumerable<TContainer>> where(std::function<bool(typename TContainer::value_type)> &&predicate) const
+    Range<WhereEnumerable<TContainer>> where(std::function<bool(typename TContainer::value_type)> &&predicate)
     {
         return Range<WhereEnumerable<TContainer>>(WhereEnumerable<TContainer>(m_container, predicate));
     }
 
-    Range<SkipEnumerable<TContainer>> skip(int count) const
+    Range<SkipEnumerable<TContainer>> skip(int count)
     {
         return Range<SkipEnumerable<TContainer>>(SkipEnumerable<TContainer>(m_container, count));
     }
 
-    Range<TakeEnumerable<TContainer>> take(int count) const
+    Range<TakeEnumerable<TContainer>> take(int count)
     {
         return Range<TakeEnumerable<TContainer>>(TakeEnumerable<TContainer>(m_container, count));
     }
@@ -206,6 +317,15 @@ class Range
 template <typename TContainer>
 class RefRange
 {
+  private:
+    template <typename GroupType>
+    Range<GroupEnumerable<RefRange<TContainer>, GroupType>> group_internal(std::function<GroupType(typename TContainer::value_type&)> predicate)
+    {
+        return Range<GroupEnumerable<RefRange<TContainer>, GroupType>>(GroupEnumerable<RefRange<TContainer>, GroupType>(*this, predicate));
+    }
+
+    TContainer &m_container;
+
   public:
     using value_type = typename TContainer::value_type;
     using iterator = typename TContainer::iterator;
@@ -215,40 +335,43 @@ class RefRange
     {
     }
 
-    iterator begin() const
+    iterator begin()
     {
         return std::begin(m_container);
     }
 
-    iterator end() const
+    iterator end()
     {
         return std::end(m_container);
     }
 
-    Range<WhereEnumerable<RefRange<TContainer>>> where(std::function<bool(typename TContainer::value_type)> &&predicate) const
+    Range<WhereEnumerable<RefRange<TContainer>>> where(std::function<bool(typename TContainer::value_type)> &&predicate)
     {
         return Range<WhereEnumerable<RefRange<TContainer>>>(WhereEnumerable<RefRange<TContainer>>(*this, predicate));
     }
 
-    Range<SkipEnumerable<RefRange<TContainer>>> skip(int count) const
+    Range<SkipEnumerable<RefRange<TContainer>>> skip(int count)
     {
         return Range<SkipEnumerable<RefRange<TContainer>>>(SkipEnumerable<RefRange<TContainer>>(*this, count));
     }
 
-    Range<TakeEnumerable<RefRange<TContainer>>> take(int count) const
+    Range<TakeEnumerable<RefRange<TContainer>>> take(int count)
     {
         return Range<TakeEnumerable<RefRange<TContainer>>>(TakeEnumerable<RefRange<TContainer>>(*this, count));
     }
 
-  private:
-    TContainer &m_container;
+    template <typename Function>
+    auto group(Function &&function) -> decltype(group_internal(toFunction(function))) 
+    {
+        return group_internal(toFunction(function));
+    }
 };
 
 template <typename TContainer>
-const RefRange<TContainer> from(TContainer &container)
+RefRange<TContainer> from(TContainer &container)
 {
     return RefRange<TContainer>{container};
 }
-}
+} // namespace range
 
 #endif
